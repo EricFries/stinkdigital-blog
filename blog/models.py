d711ebd219b89489aa0a553916072546a86bba03
EricFries <ericfries@gmail.com>
2015-06-01 19:50:09 -0400

    fix local varible issue on index page

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Author(models.Model):
-  name = models.CharField(max_length=30)
+# class Author(models.Model):
+#   name = models.CharField(max_length=30)
 
 class Post(models.Model):
-  author = models.ForeignKey(Author)
   title = models.CharField(max_length= 50)
   content = models.TextField() 
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,25 @@
 # Create your views here.
-from django.shortcuts import get_object_or_404, render
-from .models import Author, Post
+from django.shortcuts import get_object_or_404, render_to_response, render
+from django.template import RequestContext
+from .models import Post
 from django.utils import timezone
 import pdb
 
+# def index(request):
+#   # pdb.set_trace();
+#   posts = Post.objects.all()
+#   return render(request, 'blog/index.html')
+
 def index(request):
   posts = Post.objects.all()
-  return render(request, 'blog/index.html')
+  return render_to_response('blog/index.html',
+            locals(), context_instance=RequestContext(request)
+    )
 
 def new(request):
   return render(request, 'blog/new.html')
 
 def create(request):
-  # pdb.set_trace();
-  author = Author(name="Eric")
-  author.save()
   post = Post(title = request.POST['title'], content = request.POST['content'])
-  post.author = author
   post.save()
   return render(request, 'blog/index.html')
