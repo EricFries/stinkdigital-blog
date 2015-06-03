22eb0ad0753e6a2584d9862810792c8cc425b081
EricFries <ericfries@gmail.com>
2015-06-03 14:36:55 -0400

    import messages to views

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 
 
 #Post CRUD views
