febd12912e7893ce9023f7c7ce8444d6f04e648e
EricFries <ericfries@gmail.com>
2015-06-03 21:52:16 -0400

    add widget-tweaks

--- myapp/settings.py
+++ myapp/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'djangotoolbox',
     'autoload',
     'dbindexer',
+    'widget_tweaks',
     'blog',
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
